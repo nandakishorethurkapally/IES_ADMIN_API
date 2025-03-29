e5e06e5187c4f3516b2896ff9e0e61cc5bce54e6
nandakishore <nandakishoreturkapally@gmail.com>
2025-03-29 14:41:03 +0530

    ppushinn admin api

--- src/main/java/com/nandu/config/SwaggerConfig.java
+++ src/main/java/com/nandu/config/SwaggerConfig.java
@@ -1,22 +1,22 @@
-package com.nandu.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-
-
-// it will genarate documentaion for API's
-@Configuration
-@EnableSwagger2
-public class SwaggerConfig {
-	
-    @Bean
-    public Docket apiDoc() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.nandu.controller"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-}
+//package com.nandu.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//
+//
+//// it will genarate documentaion for API's
+//@Configuration
+////@EnableSwagger2
+////public class SwaggerConfig {
+////	
+////    @Bean
+////    public Docket apiDoc() {
+////        return new Docket(DocumentationType.SWAGGER_2)
+////                .select()
+////                .apis(RequestHandlerSelectors.basePackage("com.nandu.controller"))
+////                .paths(PathSelectors.any())
+////                .build();
+////    }
+////}
